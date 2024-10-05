0b7a3767dada70c73d7c96d55c8afe79e3fce775
mufaqar <mufaqar@gmail.com>
2024-10-05 19:07:41 +0500

    Provider Box

--- template-parts/box-provider.php
+++ template-parts/box-provider.php
@@ -1,13 +1,10 @@
 <div class="block rounded-xl border border-gray-100 p-3 shadow-xl transition hover:border-[#215690]/10 hover:shadow-[#215690]/10  ">
-          <a href="/providers/spectrum">
-           
-            <?php if ( has_post_thumbnail() ) {
-									the_post_thumbnail('full');
-								} else { ?>
-							<img src="<?php bloginfo('template_directory'); ?>/images/img5.jpg" alt="<?php the_title()?>" />
-							<?php } ?>
-     
-           
-            <h2 class="mt-4 text-lg  text-center"><?php the_title()?></h2>
-          </a>
-        </div>
+  <a href="<?php the_permalink() ?>">
+    <?php if (has_post_thumbnail()) {
+      the_post_thumbnail('full');
+    } else { ?>
+      <img src="<?php bloginfo('template_directory'); ?>/images/img5.jpg" alt="<?php the_title() ?>" />
+    <?php } ?>
+    <h2 class="mt-4 text-lg  text-center"><?php the_title() ?></h2>
+  </a>
+</div>
