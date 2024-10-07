e8888f8f669e3fc4c56eef9ffa329615273dcf81
syed-zunair-gillani <dev.zunaair@gmail.com>
2024-10-07 13:59:19 +0500

    figure

--- template-parts/box-provider.php
+++ template-parts/box-provider.php
@@ -3,8 +3,10 @@
     <?php if (has_post_thumbnail()) {
       the_post_thumbnail('full');
     } else { ?>
-      <img src="<?php bloginfo('template_directory'); ?>/images/img5.jpg" alt="<?php the_title() ?>" class="w-[144px]" />
+      <figure class="max-w-[144px]">
+        <img src="<?php bloginfo('template_directory'); ?>/images/img5.jpg" alt="<?php the_title() ?>" class="w-[144px]" />
+      </figure>
     <?php } ?>
-    <h2 class="mt-4 text-lg  text-center"><?php the_title() ?></h2>
+    <h2 class="mt-4 text-lg text-center"><?php the_title() ?></h2>
   </a>
 </div>
