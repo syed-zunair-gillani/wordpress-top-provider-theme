dbacf06f8ed7f32a088088b56b4059f369fd340e
mufaqar <mufaqar@gmail.com>
2024-10-08 12:32:47 +0500

    final theme

--- inc/extra_functions.php
+++ inc/extra_functions.php
@@ -1,4 +1,73 @@
 <?php
 
+
+
 include_once('cpts.php');
 include_once('rules.php');
+
+if ( ! defined( '_S_VERSION' ) ) {
+	// Replace the version number of the theme on each release.
+	define( '_S_VERSION', '1.0.0' );
+}
+
+
+function cbl_theme_setup() {
+	
+	load_theme_textdomain( 'cbl_theme', get_template_directory() . '/languages' );
+	add_theme_support( 'automatic-feed-links' );
+	add_theme_support( 'title-tag' );
+	add_theme_support( 'post-thumbnails' );
+	register_nav_menus(
+		array(
+			'main' => esc_html__( 'Main', 'cbl_theme' ),
+			'footer' => esc_html__( 'Footer', 'cbl_theme' ),
+		)
+	);
+	add_theme_support(
+		'html5',
+		array(
+			'search-form',
+			'comment-form',
+			'comment-list',
+			'gallery',
+			'caption',
+			'style',
+			'script',
+		)
+	);
+
+
+}
+add_action( 'after_setup_theme', 'cbl_theme_setup' );
+
+/**
+ * Set the content width in pixels, based on the theme's design and stylesheet.
+ *
+ * Priority 0 to make it available to lower priority callbacks.
+ *
+ * @global int $content_width
+ */
+function cbl_theme_content_width() {
+	$GLOBALS['content_width'] = apply_filters( 'cbl_theme_content_width', 640 );
+}
+add_action( 'after_setup_theme', 'cbl_theme_content_width', 0 );
+
+/**
+ * Register widget area.
+ *
+ * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
+ */
+function cbl_theme_widgets_init() {
+	register_sidebar(
+		array(
+			'name'          => esc_html__( 'Sidebar', 'cbl_theme' ),
+			'id'            => 'sidebar-1',
+			'description'   => esc_html__( 'Add widgets here.', 'cbl_theme' ),
+			'before_widget' => '<section id="%1$s" class="widget %2$s">',
+			'after_widget'  => '</section>',
+			'before_title'  => '<h2 class="widget-title">',
+			'after_title'   => '</h2>',
+		)
+	);
+}
+add_action( 'widgets_init', 'cbl_theme_widgets_init' );
