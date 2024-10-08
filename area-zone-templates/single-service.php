d67c5d3d61d75ded1b882d9ef29d64c64b584ff0
mufaqar <mufaqar@gmail.com>
2024-10-08 12:14:49 +0500

    Service Template

--- area-zone-templates/single-service.php
+++ area-zone-templates/single-service.php
@@ -1,1 +1,20 @@
-<p>Service</p>
+<?php get_header();
+    $url_path = trim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), '/');
+    $parts = explode('/', $url_path);
+    $last_part = strtoupper(array_pop($parts));
+?>
+
+<section class="min-h-[40vh] flex items-center bg-gray-50">
+    <div class="container mx-auto px-4">
+        <div class="flex justify-center flex-col items-center">
+            <h1 class="sm:text-5xl text-2xl font-bold text-center max-w-[850px] mx-auto capitalize leading-10">
+                Search <?php echo $last_part ?> Service Providers
+            </h1>
+            <p class="text-xl text-center font-[Roboto] my-5">Enter your zip so we can find the best <?php echo $last_part ?> Providers in your area:</p>
+            <?php get_template_part('template-parts/filter', 'form'); ?>
+        </div>
+    </div>
+</section>
+
+
+<?php get_footer() ?>
