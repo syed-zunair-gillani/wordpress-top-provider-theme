fb9be068518f8578e977d940beeb3bd6ead29441
mufaqar <mufaqar@gmail.com>
2024-10-09 18:44:33 +0500

    provider card

--- area-zone-templates/single-service-zone-state-zone-city-post.php
+++ area-zone-templates/single-service-zone-state-zone-city-post.php
@@ -10,8 +10,6 @@
 get_header();
 // Define the zip code you want to search for
 $target_zipcode = '90001';
-
-// Arguments for WP_Query
 $args = array(
     'post_type' => 'providers', // Custom post type slug
     'posts_per_page' => -1,     // Get all matching posts
@@ -24,89 +22,7 @@ $args = array(
     ),
 );
 
-// Query the posts
-$query = new WP_Query($args);
 
-// Check if any posts were found
-if ($query->have_posts()) {
-    echo '<h3>Providers with Zip Code ' . esc_html($target_zipcode) . ':</h3>';
-    // Loop through matching posts and display their titles
-    while ($query->have_posts()) {
-        $query->the_post();
-        
-        ?>
-        <div class="grid gap-7">
-            <div class="w-full h-auto shadow-xl border rounded-t-md rounded-b-md flex flex-col">
-                <div class="md:w-full min-w-fit bg-[#215690] flex justify-between items-center">
-                    <h2 class="text-base font-bold text-center text-white p-5"><span> 1 </span>- <?php the_title()?></h2>
-                    <h2 class="text-base font-bold text-center text-white p-5"></h2>
-                </div>
-                <div class="md:w-full w-full grid grid-cols-1 dtable md:grid-cols-5 flex flex-col">
-                    <div class="md:border-r border-r-0 md:border-b-0 border-b grid items-center justify-center p-5">
-                        <a target="_blank" href="/providers/xfinity">
-                            <img
-                                alt="Feature Image"
-                                loading="lazy"
-                                width="140"
-                                height="50"
-                                decoding="async"
-                                data-nimg="1"
-                                src="https://www.cablemovers.net/_next/image?url=https%3A%2F%2Fcblproject.cablemovers.net%2Fwp-content%2Fuploads%2F2023%2F08%2Fxfinity.jpg&w=256&q=75"
-                                style="color: transparent;"
-                            />
-                        </a>
-                    </div>
-                    <div class="md:border-r border-r-0 md:border-b-0 border-b grid items-center justify-center p-5">
-                        <div class="text-center">
-                            <p class="tch">Speeds from</p>
-                            <p class="tcd">75-1200 Mbps</p>
-                        </div>
-                    </div>
-                    <div class="md:border-r border-r-0 md:border-b-0 border-b grid items-center justify-center p-5 px-3">
-                        <ul class="grid items-center justify-center">
-                            <li class="flex gap-2 items-center">
-                                <svg class="min-w-[1rem] h-4 text-[#ef9831] font-extrabold" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="4" d="M5 13l4 4L19 7"></path>
-                                </svg>
-                                <span class="text-sm">Fast and secure</span>
-                            </li>
-                            <li class="flex gap-2 items-center">
-                                <svg class="min-w-[1rem] h-4 text-[#ef9831] font-extrabold" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="4" d="M5 13l4 4L19 7"></path>
-                                </svg>
-                                <span class="text-sm">99.9% network reliability</span>
-                            </li>
-                            <li class="flex gap-2 items-center">
-                                <svg class="min-w-[1rem] h-4 text-[#ef9831] font-extrabold" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="4" d="M5 13l4 4L19 7"></path>
-                                </svg>
-                                <span class="text-sm">Wall-to-wall WiFi coverage with xFi </span>
-                            </li>
-                        </ul>
-                    </div>
-                    <div class="md:border-r border-r-0 md:border-b-0 border-b grid items-center justify-center p-5">
-                        <div>
-                            <p class="tch">Pricing starts from</p>
-                            <p class="tcd"><span class="font-extrabold text-[#215690] font-[Roboto] text-xl"> $19.99 </span> /mo.</p>
-                        </div>
-                    </div>
-                    <div class="grid gap-3 items-center justify-center p-5">
-                        <a class="text-base text-white font-[Roboto] uppercase px-5 py-2.5 bg-[#215690] hover:bg-[#ef9831]" href="tel:844-581-1129">844-581-1129 </a>
-                        <a class="text-base text-white font-[Roboto] uppercase px-5 py-2.5 bg-[#ef9831] hover:bg-[#215690]" href="/providers/xfinity">View Plans</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <?php 
-
-
-    }
-} else {
-    echo 'No providers found with the specified zip code.';
-}
-
-// Reset post data
-wp_reset_postdata();
 ?>
 
 
@@ -151,68 +67,20 @@ wp_reset_postdata();
         <div class="mb-10">
             <h2 class="text-2xl font-bold capitalize leading-10">Internet Providers in <span class="text-[#ef9831]">20001 </span></h2>
         </div>
-        <div class="grid gap-7">
-            <div class="w-full h-auto shadow-xl border rounded-t-md rounded-b-md flex flex-col">
-                <div class="md:w-full min-w-fit bg-[#215690] flex justify-between items-center">
-                    <h2 class="text-base font-bold text-center text-white p-5"><span> 1 </span>- Xfinity</h2>
-                    <h2 class="text-base font-bold text-center text-white p-5"></h2>
-                </div>
-                <div class="md:w-full w-full grid grid-cols-1 dtable md:grid-cols-5 flex flex-col">
-                    <div class="md:border-r border-r-0 md:border-b-0 border-b grid items-center justify-center p-5">
-                        <a target="_blank" href="/providers/xfinity">
-                            <img
-                                alt="Feature Image"
-                                loading="lazy"
-                                width="140"
-                                height="50"
-                                decoding="async"
-                                data-nimg="1"
-                                src="https://www.cablemovers.net/_next/image?url=https%3A%2F%2Fcblproject.cablemovers.net%2Fwp-content%2Fuploads%2F2023%2F08%2Fxfinity.jpg&w=256&q=75"
-                                style="color: transparent;"
-                            />
-                        </a>
-                    </div>
-                    <div class="md:border-r border-r-0 md:border-b-0 border-b grid items-center justify-center p-5">
-                        <div class="text-center">
-                            <p class="tch">Speeds from</p>
-                            <p class="tcd">75-1200 Mbps</p>
-                        </div>
-                    </div>
-                    <div class="md:border-r border-r-0 md:border-b-0 border-b grid items-center justify-center p-5 px-3">
-                        <ul class="grid items-center justify-center">
-                            <li class="flex gap-2 items-center">
-                                <svg class="min-w-[1rem] h-4 text-[#ef9831] font-extrabold" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="4" d="M5 13l4 4L19 7"></path>
-                                </svg>
-                                <span class="text-sm">Fast and secure</span>
-                            </li>
-                            <li class="flex gap-2 items-center">
-                                <svg class="min-w-[1rem] h-4 text-[#ef9831] font-extrabold" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="4" d="M5 13l4 4L19 7"></path>
-                                </svg>
-                                <span class="text-sm">99.9% network reliability</span>
-                            </li>
-                            <li class="flex gap-2 items-center">
-                                <svg class="min-w-[1rem] h-4 text-[#ef9831] font-extrabold" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="4" d="M5 13l4 4L19 7"></path>
-                                </svg>
-                                <span class="text-sm">Wall-to-wall WiFi coverage with xFi </span>
-                            </li>
-                        </ul>
-                    </div>
-                    <div class="md:border-r border-r-0 md:border-b-0 border-b grid items-center justify-center p-5">
-                        <div>
-                            <p class="tch">Pricing starts from</p>
-                            <p class="tcd"><span class="font-extrabold text-[#215690] font-[Roboto] text-xl"> $19.99 </span> /mo.</p>
-                        </div>
-                    </div>
-                    <div class="grid gap-3 items-center justify-center p-5">
-                        <a class="text-base text-white font-[Roboto] uppercase px-5 py-2.5 bg-[#215690] hover:bg-[#ef9831]" href="tel:844-581-1129">844-581-1129 </a>
-                        <a class="text-base text-white font-[Roboto] uppercase px-5 py-2.5 bg-[#ef9831] hover:bg-[#215690]" href="/providers/xfinity">View Plans</a>
-                    </div>
-                </div>
-            </div>
-        </div>
+        <?php
+        // Query the posts
+            $query = new WP_Query($args);
+            if ($query->have_posts()) {
+                while ($query->have_posts()) {
+                    $query->the_post();        
+                    get_template_part( 'template-parts/provider', 'card' );
+                }
+            } else {
+                echo 'No providers found with the specified zip code.';
+            }
+            // Reset post data
+            wp_reset_postdata();
+        ?>
         <div><p class="text-sm font-[Roboto] mt-10">*DISCLAIMER: Availability vary by service address. not all offers available in all areas, pricing subject to change at any time. Additional taxes, fees, and terms may apply.</p></div>
     </div>
 </section>
