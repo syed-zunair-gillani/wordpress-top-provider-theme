693a032ea9e571b3ac644b2ec3c85bab1032e7c0
mufaqar <mufaqar@gmail.com>
2024-10-29 12:53:53 +0500

    Providers

--- temp-providers.php
+++ temp-providers.php
@@ -85,6 +85,39 @@ get_header();
 
             </div>
         </div>
+
+        <div class="max-w-[1110px] w-full mx-auto h-[1px] bg-black/20 my-16"></div>
+
+        <div>
+            <h3 class="text-3xl font-semibold mb-10">Top Home Security System Providers</h3>
+            <div class="grid sm:!grid-cols-2 md:!grid-cols-4 grid-cols-1 gap-7">
+            <?php
+                // Arguments for the WP Query
+                $args = array(
+                    'post_type'      => 'providers', // Custom post type name
+                    'posts_per_page' => -1, // Number of posts to display
+                    'order'          => 'DESC', // Order of the posts
+                    'providers_types'        => 'home-security' // Order by date
+                );
+
+                // Custom query to fetch posts
+                $providers_query = new WP_Query($args);
+
+                // The Loop
+                if ($providers_query->have_posts()) :
+                    while ($providers_query->have_posts()) : $providers_query->the_post();
+                        get_template_part('template-parts/box', 'provider');
+                    endwhile;
+                else :
+                    echo '<p>No providers found.</p>';
+                endif;
+
+                // Reset post data to avoid conflicts
+                wp_reset_postdata();
+                ?>
+
+            </div>
+        </div>
     </div>
 </section>
 
