8c800468eb4d8cf7efbbcea6c857b11661ae3c64
mufaqar <mufaqar@gmail.com>
2024-11-05 12:28:54 +0500

    box

--- template-parts/box-provider_slide.php
+++ template-parts/box-provider_slide.php
@@ -1,11 +1,8 @@
-
-  
-        <div class="slide-item">
+<div class="slide-item">
             <a href="<?php echo esc_url(get_permalink()); ?>">
                 <?php if (has_post_thumbnail()) : ?>
                     <?php the_post_thumbnail('full'); ?>
                 <?php endif; ?>
-                <h2 class="mt-4 text-lg text-center"><?php the_title(); ?></h2>
-            </a>
-        </div>
+             </a>
+ </div>
  
