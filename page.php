baf15ed0c544e6b651a113172c05a9b80d8f4ec4
mufaqar <mufaqar@gmail.com>
2024-11-07 14:29:47 +0500

    Footer

--- page.php
+++ page.php
@@ -14,27 +14,29 @@
 
 get_header();
 ?>
+<section class="py-24 bg-[#215690]">
+    <div class="container mx-auto px-4">
+        <h1 class="sm:text-5xl text-4xl leading-normal font-extrabold text-white text-center"><?php the_title()?></h1>
+    </div>
+</section>
 
-	<main id="primary" class="site-main">
+<section class="py-16">
+    <div class="container mx-auto px-4">
 
 		<?php
 		while ( have_posts() ) :
 			the_post();
 
-			get_template_part( 'template-parts/content', 'page' );
-
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
+			the_content();
 
 		endwhile; // End of the loop.
 		?>
 
-	</main><!-- #main -->
+</div>
+</section>
 
 <?php
-get_sidebar();
+
 get_footer();
 
 
