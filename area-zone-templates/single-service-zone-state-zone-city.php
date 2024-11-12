578e0033bb2d8fc72e378060918a32c8272bdf73
mufaqar <mufaqar@gmail.com>
2024-11-12 21:59:24 +0500

    TV

--- area-zone-templates/single-service-zone-state-zone-city.php
+++ area-zone-templates/single-service-zone-state-zone-city.php
@@ -746,54 +746,61 @@
 </section>
 
 
-<section class="my-16">
-    <div class="container mx-auto px-4">
-        <div class="mb-10">
-            <h2 class="text-2xl font-bold mb-2">
-            What Local Channels are Available from Cable TV Providers in  <span class="text-[#ef9831]"><?php echo $city?>, <span class="uppercase"><?php echo $state?></span></span>
-            </h2>
-            <p class="PClass">
-            The best part of using a cable provider is accessing the news, sports, events, and special screenings of shows in your regional location. Many fans love watching the local college team take on a long-term rival but get locked out because streaming services won’t cover the event.</p>
-            <p class="PClass">
-                Local channels are based on the service provider you choose for your TV service. <a href="https://www.channelmaster.com/pages/free-tv-channels">Click here </a>to find the list of local stations you’ll get while living in Glendale CA.
-                </p>
-        </div>
 
-        <div class="mb-10">
-            <h2 class="text-2xl font-bold mb-2">
-            Why Use Cable Companies in   <span class="text-[#ef9831]"><?php echo $city?>, <span class="uppercase"><?php echo $state?></span></span>
-            </h2>
-            <p class="PClass">
-                  Using all the streaming services like Netflix and Hulu sounds like a great deal, but it starts to add up over time. Instead of saving money, you’re paying more for endless streams, with prices only getting higher. 
-             </p>
-             <p class="PClass">
-                   A quality cable TV provider offers the ability to DVR different shows, watch local events and news, and uncover new channels you may have never seen before. Instead of needing 5-8 different streaming services to watch your favorite shows, you can use the provided remote to hop from CNN to Nickelodeon to ESPN to a local news station.   </p>
-             <p class="PClass">
-               Glendale offers many top-ranked TV providers, with Spectrum, Dish, and DirecTV leading the charge. Each one provides competitive pricing and an expansive content library of channels to surf when relaxing.  
-             </p>
-        </div>
+    <?php 
+    if ($type === 'tv'): ?>
+            <section class="my-16">
+                <div class="container mx-auto px-4">
+                    <div class="mb-10">
+                        <h2 class="text-2xl font-bold mb-2">
+                        What Local Channels are Available from Cable TV Providers in  <span class="text-[#ef9831]"><?php echo $city?>, <span class="uppercase"><?php echo $state?></span></span>
+                        </h2>
+                        <p class="PClass">
+                        The best part of using a cable provider is accessing the news, sports, events, and special screenings of shows in your regional location. Many fans love watching the local college team take on a long-term rival but get locked out because streaming services won’t cover the event.</p>
+                        <p class="PClass">
+                            Local channels are based on the service provider you choose for your TV service. <a href="https://www.channelmaster.com/pages/free-tv-channels">Click here </a>to find the list of local stations you’ll get while living in Glendale CA.
+                            </p>
+                    </div>
 
-        <div class="mb-10">
-            <h2 class="text-2xl font-bold mb-2">
-            How We Research TV Companies in   <span class="text-[#ef9831]"><?php echo $city?>, <span class="uppercase"><?php echo $state?></span></span>
-            </h2>
-            <p class="PClass">
-            To ensure we provide the best details on different cable TV providers in the area, we look at a list of specific factors. These are the details we find new customers and recently moved families want the most. That may include: 
-            </p>
-            <ul class="text-base">
-                <li>Type of Service: Whether the cable TV provider works from satellite, traditional coax cable, or a digital service attached to your internet or mobile device. </li>
-                <li>Length of Contract: Most cable TV providers working with internet or mobile streaming require a long-term commitment.         </li>
-                <li>Video Quality: How sharp is the image? Will it maintain that quality during storms or heavy use due to the high demand of all customers?         </li>
-                <li>Accessories: What equipment and features will the cable TV provider offer, like remotes, DVRs, devices, and more?         </li>
-                <li>Transparent Pricing: Are there any hidden fees that will increase the initial offer price when you sign up?         </li>
-                <li>Special Promotions: Can you secure a monthly discount due to being a new customer or reduce your contract when bundling with other services?         </li>
-                <li>Real Customer Support: When you have a question or need help, you reach a live human being for support. </li>
+                    <div class="mb-10">
+                        <h2 class="text-2xl font-bold mb-2">
+                        Why Use Cable Companies in   <span class="text-[#ef9831]"><?php echo $city?>, <span class="uppercase"><?php echo $state?></span></span>
+                        </h2>
+                        <p class="PClass">
+                            Using all the streaming services like Netflix and Hulu sounds like a great deal, but it starts to add up over time. Instead of saving money, you’re paying more for endless streams, with prices only getting higher. 
+                        </p>
+                        <p class="PClass">
+                            A quality cable TV provider offers the ability to DVR different shows, watch local events and news, and uncover new channels you may have never seen before. Instead of needing 5-8 different streaming services to watch your favorite shows, you can use the provided remote to hop from CNN to Nickelodeon to ESPN to a local news station.   </p>
+                        <p class="PClass">
+                        Glendale offers many top-ranked TV providers, with Spectrum, Dish, and DirecTV leading the charge. Each one provides competitive pricing and an expansive content library of channels to surf when relaxing.  
+                        </p>
+                    </div>
 
-        </ul>
-        </div>
-       
-    </div>
-</section>
+                    <div class="mb-10">
+                        <h2 class="text-2xl font-bold mb-2">
+                        How We Research TV Companies in   <span class="text-[#ef9831]"><?php echo $city?>, <span class="uppercase"><?php echo $state?></span></span>
+                        </h2>
+                        <p class="PClass">
+                        To ensure we provide the best details on different cable TV providers in the area, we look at a list of specific factors. These are the details we find new customers and recently moved families want the most. That may include: 
+                        </p>
+                        <ul class="text-base">
+                            <li>Type of Service: Whether the cable TV provider works from satellite, traditional coax cable, or a digital service attached to your internet or mobile device. </li>
+                            <li>Length of Contract: Most cable TV providers working with internet or mobile streaming require a long-term commitment.         </li>
+                            <li>Video Quality: How sharp is the image? Will it maintain that quality during storms or heavy use due to the high demand of all customers?         </li>
+                            <li>Accessories: What equipment and features will the cable TV provider offer, like remotes, DVRs, devices, and more?         </li>
+                            <li>Transparent Pricing: Are there any hidden fees that will increase the initial offer price when you sign up?         </li>
+                            <li>Special Promotions: Can you secure a monthly discount due to being a new customer or reduce your contract when bundling with other services?         </li>
+                            <li>Real Customer Support: When you have a question or need help, you reach a live human being for support. </li>
+
+                    </ul>
+                    </div>
+                
+                </div>
+            </section>
+                       
+    <?php endif; ?>
+
+
 
 
 
