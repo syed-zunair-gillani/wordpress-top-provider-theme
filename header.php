2a763ac7ff7644a72f4b7eb97d336b61e2903cbb
mufaqar <mufaqar@gmail.com>
2024-11-22 19:35:12 +0500

    CD

--- header.php
+++ header.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html <?php language_attributes(); ?>>
 <head>
-    <meta name="robots" content="noindex, nofollow" />
+    <!-- <meta name="robots" content="noindex, nofollow" /> -->
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
