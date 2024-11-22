74f62d5b9304abea8c51b0c618e0c10ac643c170
mufaqar <mufaqar@gmail.com>
2024-11-22 19:42:25 +0500

    CD

--- area-zone-templates/single-service-zone-state-zone-city-post.php
+++ area-zone-templates/single-service-zone-state-zone-city-post.php
@@ -12,13 +12,13 @@ $type =$wp_query->query_vars['service'];
 add_filter('wpseo_title', 'Generate_Title_For_Zipcode');
 add_filter('wpseo_metadesc', 'Generate_Description_For_Zipcode');
 
-function Generate_Canonical_Tag($canonical) {
+function Generate_Canonical($canonical) {
     if (is_page('example-page')) {
         return 'https://example.com/custom-canonical-url/';
     }
     return $canonical; // Default value
 }
-add_filter('wpseo_canonical', 'Generate_Canonical_Tag', 10);
+add_filter('wpseo_canonical', 'Generate_Canonical', 10);
 
 
 get_header(); ?>
